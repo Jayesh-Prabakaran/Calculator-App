72845c8ec5e45d8636fe89b20f4bc066fe439dcb
Jayesh Prabakaran <sjayeshprabu@gmail.com>
2022-11-09 12:03:26 +0530

    Added the main function of the calculator app

---
+++ Main/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("WELCOME TO THE CALCULATOR APP!");
+
+        System.out.println("Enter value 1: ");
+        int val1=sc.nextInt();
+
+        System.out.println("Enter value 2: ");
+        int val2=sc.nextInt();
+    }
+}
