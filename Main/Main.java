a3373c48343c4b0b8398d50287525c977096790c
Jayesh Prabakaran <sjayeshprabu@gmail.com>
2022-11-09 12:40:31 +0530

    Added the sum function body

--- Main/Main.java
+++ Main/Main.java
@@ -2,18 +2,23 @@ import java.util.Scanner;
 
 public class Main
 {
+    public static void sum(int val1, int val2)
+    {
+        System.out.println(val1+" + "+val2+" = "+(val1+val2));
+    }
+
     public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("WELCOME TO THE CALCULATOR APP!");
 
         System.out.println("Enter value 1: ");
-        int val1=sc.nextInt();
+        int val1 = sc.nextInt();
 
         System.out.println("Enter value 2: ");
-        int val2=sc.nextInt();
-        
+        int val2 = sc.nextInt();
+
         sum(val1, val2);
         difference(val1, val2);
     }
